5e94ba022f970ba99b3a470edb756772d6b4b8cf
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-03-09 00:45:20 +0900

    ⚡️refactor : 네이밍 변경, ViewModel 및 Activity 수정

--- app/src/main/java/com/hy0417sage/wirebarley/presentation/view/MainViewModel.kt
+++ app/src/main/java/com/hy0417sage/wirebarley/presentation/view/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.hy0417sage.wirebarley.presentation.view
 
 import androidx.lifecycle.*
+import com.hy0417sage.wirebarley.data.model.ViewData
 import com.hy0417sage.wirebarley.domain.ExchangeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,10 +11,10 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val exchangeRepository: ExchangeRepository,
 ) : ViewModel() {
+
     private val _quotes = MutableLiveData<List<Double>>()
-    val quotes: LiveData<List<Double>> get() = _quotes
-    private val _now = MutableLiveData<Int>(0)
-    val now: LiveData<Int> get() = _now
+    private val _viewData = MutableLiveData<ViewData>()
+    val viewData: LiveData<ViewData> get() = _viewData
 
     init {
         loadAPI()
@@ -25,10 +26,22 @@ class MainViewModel @Inject constructor(
             val exchangeRateList = listOf(quotes.KRW, quotes.JPY, quotes.PHP)
             _quotes.value = exchangeRateList
             _quotes.postValue(exchangeRateList)
+            _viewData.value = ViewData(0, exchangeRateList[0], 0.0)
         }
     }
 
-    fun changeExchangeRate(which: Int) {
-        _now.value = which
+    fun exchangeRate(index: Int, reset: Double?) {
+        val exchangeRate = _quotes.value?.get(index) ?: 0.0
+        val total = viewData.value?.total ?: (reset ?: 0.0)
+        _viewData.value = ViewData(index, exchangeRate, total)
+        _viewData.postValue(ViewData(index, exchangeRate, total))
+    }
+
+    fun totalExchange(edit: String?) {
+        val index = viewData.value?.nation ?: 0
+        val exchangeRate = viewData.value?.exchangeRate ?: 0.0
+        val total = (viewData.value?.exchangeRate ?: 0.0) * (edit?.toDouble() ?: 0.0)
+        _viewData.value = ViewData(index, exchangeRate, total)
+        _viewData.postValue(ViewData(index, exchangeRate, total))
     }
 }

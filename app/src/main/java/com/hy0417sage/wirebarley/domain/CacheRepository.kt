2bfb4e9904538bf3ee1589f3631bfd088f9349c1
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-03-09 19:35:59 +0900

    ⚡️refactor : 캐시 코드 삭제

--- app/src/main/java/com/hy0417sage/wirebarley/domain/CacheRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.hy0417sage.wirebarley.domain
-
-import com.hy0417sage.wirebarley.data.model.CacheEntity
-
-interface CacheRepository {
-    fun getCache(): CacheEntity
-    suspend fun updateCache(cache: CacheEntity)
-}
